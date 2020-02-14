385b392be429ad3402c64745d24c871ef820f1ed
suika p <nekomikan49@gmail.com>
2020-02-14 10:27:53 +0900

    sample

---
+++ art_frame/pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+
+<nav class="navbar is-light" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a class="navbar-item">
+    </a>
+  </div>
+  <div class="navbar-menu">
+    <div class="navbar-start">
+      <a class="navbar-item" href="/index">
+        Attend
+      </a>
+      <a class="navbar-item" href="/profile">
+        Member
+      </a>
+    </div>
+  </div>
+
+</nav>
+
+<iframe src="~/static/code/art.html" width="600" height="400"></iframe>
+
+</div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
+
